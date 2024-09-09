862971d17f4f40cd8b4b63c696ca5dd010043338
Tac Tacelosky <tacman@gmail.com>
2024-09-09 06:52:45 -0400

    remove bootstrap css

--- assets/app.js
+++ assets/app.js
@@ -5,7 +5,6 @@ import '@tabler/core'; // the js?
 // import 'bootstrap'; // bootstrap js is included in @tabler/core, don't load twice!
 
 // css
-import 'bootstrap/dist/css/bootstrap.min.css'; // get bootstrap js
 import '@tabler/core/dist/css/tabler.min.css'
 
 
